718bcc698fe852a349886f3eb336d4f423a99ce0
x404 <kronos2003@gmail.com>
2017-01-11 16:27:02 +0200

    Первое письмо готово

--- mail.php
+++ mail.php
@@ -1,16 +1,10 @@
 <?php
-
-// Для отправки HTML-письма должен быть установлен заголовок Content-type
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-
-$to = 'info@proverstka.com.ua'; // обратите внимание на запятую
+$to = 'info@proverstka.com.ua';
 $subject = 'Письмо 1';
 
 $message = file_get_contents("1.txt");
-
-
-
 mail($to, $subject, $message, $headers);
 ?>
 
